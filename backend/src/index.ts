1c7d272525035384f7ab2cae1c5675f85d20fa4c
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-10-12 20:52:22 +0200

    connection to db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
+import connectDB from './config/db'
+import dotenv from 'dotenv'
+
+dotenv.config()
 const app = express()
+connectDB()
 
 app.get("/", (req: Request, res: Response) => {
     res.send("I am here")
 })
 
-const PORT = 4321;
+const PORT = process.env.PORT || 4321;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
